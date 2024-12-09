9b68b39d102992672bd4874becf8757b9355e3a5
BugHunter-X <askzenai@gmail.com>
2024-12-09 19:42:07 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,30 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+      {/* This is where the error occurs. It will cause a runtime error. */}
+      <img src="/not-existing.jpg" alt="Not existing image" />
+    </div>
+  );
+}
+
+export default About;
+```
